1fc4597b32cf60a26396581ee44477ec96c47506
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-03-22 10:37:34 +0530

    Create reload_page.ts

---
+++ reload_page.ts
@@ -0,0 +1,22 @@
+////////////////////////////////////
+
+reloadCurrentRoute() {
+    let currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router.navigate([currentUrl]);
+    });
+}
+
+
+//////////////////////////////
+
+ reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
+  }
+
+
+/////////////////////////////
+window.location.reload();
