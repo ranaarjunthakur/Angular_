d6a7ab2ba143879a1c5bda639197eb9e32963734
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-10-30 12:15:39 +0530

    Create 1. UppercaseFirstPipe.ts

---
+++ pipes/1. UppercaseFirstPipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'uppercaseFirst' })
+export class UppercaseFirstPipe implements PipeTransform {
+  transform(value: string): string {
+    if (!value) return value;
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+  }
+}
+
+
+
+<p>{{ 'hello world' | uppercaseFirst }}</p>
+<!-- Output: "Hello world" -->
